56adebf6f76830a6a7cbf555be4933a08f72a4b5
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-02-14 19:29:20 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,79 +7,79 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
     <>
-    <section class="pricing py-2">
-    <h1 class="text-center pb-3">Pricing Table</h1>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="card mb-5 mb-lg-0">
-            <div class="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
-              <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
+    <section className="pricing py-2">
+    <h1 className="text-center pb-3">Pricing Table</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="card mb-5 mb-lg-0">
+            <div className="card-body">
+              <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
+              <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
               <hr />
-              <ul class="fa-ul">
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Single User</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>5GB Storage</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
-                <li class="text-muted"><span class="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Unlimited
+              <ul className="fa-ul">
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Single User</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>5GB Storage</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
+                <li className="text-muted"><span className="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Unlimited
                   Private Projects</li>
-                <li class="text-muted"><span class="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Dedicated
+                <li className="text-muted"><span className="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Dedicated
                   Phone Support</li>
-                <li class="text-muted"><span class="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Free Subdomain
+                <li className="text-muted"><span className="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Free Subdomain
                 </li>
-                <li class="text-muted"><span class="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Monthly Status
+                <li className="text-muted"><span className="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Monthly Status
                   Reports</li>
               </ul>
-              <div class="d-grid">
-                <a href="#" class="btn btn-primary text-uppercase">Button</a>
+              <div className="d-grid">
+                <a href="#" className="btn btn-primary text-uppercase">Button</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="card mb-5 mb-lg-0">
-            <div class="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">Plus</h5>
-              <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
+        <div className="col-lg-4">
+          <div className="card mb-5 mb-lg-0">
+            <div className="card-body">
+              <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
+              <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
               <hr />
-              <ul class="fa-ul">
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span><strong>5 Users</strong></li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>50GB Storage</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Private Projects</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Dedicated Phone Support</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Free Subdomain</li>
-                <li class="text-muted"><span class="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Monthly Status
+              <ul className="fa-ul">
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span><strong>5 Users</strong></li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>50GB Storage</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Private Projects</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Dedicated Phone Support</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Free Subdomain</li>
+                <li className="text-muted"><span className="fa-li"> <FontAwesomeIcon icon={faTimes} /></span>Monthly Status
                   Reports</li>
               </ul>
-              <div class="d-grid">
-                <a href="#" class="btn btn-primary text-uppercase">Button</a>
+              <div className="d-grid">
+                <a href="#" className="btn btn-primary text-uppercase">Button</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">Pro</h5>
-              <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
+        <div className="col-lg-4">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
+              <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
               <hr />
-              <ul class="fa-ul">
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span><strong>Unlimited Users</strong>
+              <ul className="fa-ul">
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span><strong>Unlimited Users</strong>
                 </li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>150GB Storage</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Private Projects</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Dedicated Phone Support</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span><strong>Unlimited</strong> Free
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>150GB Storage</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Public Projects</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Community Access</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Unlimited Private Projects</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Dedicated Phone Support</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span><strong>Unlimited</strong> Free
                   Subdomains</li>
-                <li><span class="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Monthly Status Reports</li>
+                <li><span className="fa-li"><FontAwesomeIcon icon={faCheck} /></span>Monthly Status Reports</li>
               </ul>
-              <div class="d-grid">
-                <a href="#" class="btn btn-primary text-uppercase">Button</a>
+              <div className="d-grid">
+                <a href="#" className="btn btn-primary text-uppercase">Button</a>
               </div>
             </div>
           </div>
